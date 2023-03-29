c0db54e2a0bd51958c4e12c5b2780354f68a50d9
Joy Duangta <supanida_duangta@hotmaail.co.th>
2023-03-30 04:11:37 +0700

    FINISH: Your Project (3 project)

--- lib/main.dart
+++ lib/main.dart
@@ -39,82 +39,90 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.chevron_left),
-          color: Colors.black,
-        ),
-        title: Container(
-          margin: const EdgeInsets.only(top: 6.0),
-          child: Image.asset(
-            'assets/img/Logo.png',
-            fit: BoxFit.contain,
-            scale: 8,
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.chevron_left),
+            color: Colors.black,
           ),
-        ),
-        centerTitle: true,
-      ),
-      body: Stack(
-        children: [
-          Container(
-            margin:
-                const EdgeInsets.only(left: 20, right: 20, top: 50, bottom: 20),
-            color: Colors.white,
+          title: Container(
+            margin: const EdgeInsets.only(top: 6.0),
+            child: Image.asset(
+              'assets/img/Logo.png',
+              fit: BoxFit.contain,
+              scale: 8,
+            ),
           ),
-          Container(
-            alignment: Alignment.topCenter,
-            child: Card(
-              margin: const EdgeInsets.all(20),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50)),
-              color: const Color(0xff387AC4),
-              child: SizedBox(
-                width: 200,
-                height: 50,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Text(
-                      "โครงการของคุณ",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ],
+          centerTitle: true,
+        ),
+        body: Stack(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(
+                  left: 20, right: 20, top: 50, bottom: 20),
+              color: Colors.white,
+            ),
+            Container(
+              alignment: Alignment.topCenter,
+              child: Card(
+                margin: const EdgeInsets.all(20),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50)),
+                color: const Color(0xff387AC4),
+                child: SizedBox(
+                  width: 200,
+                  height: 50,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        "โครงการของคุณ",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          Container(
-            margin:
-                const EdgeInsets.only(top: 90, left: 40, right: 40, bottom: 20),
-            child: ListView.builder(
-              itemCount: 3,
-              itemBuilder: (context, index) {
-                return Card(
-                    child: ListTile(
-                  leading: const Icon(
-                    Icons.location_pin,
-                    color: Colors.red,
-                    size: 30,
-                  ),
-                  title: Text("ชื่อโครงการ $index"),
-                  // subtitle: selectedIndex == index
-                  //     ? const Text("ปิดโครงการ")
-                  //     : null),
-                ));
-              },
+            Container(
+              margin: const EdgeInsets.only(
+                  top: 90, left: 40, right: 40, bottom: 20),
+              child: ListView.builder(
+                itemCount: 3,
+                itemBuilder: (context, index) {
+                  return Card(
+                      child: ListTile(
+                    leading: const Icon(
+                      Icons.location_pin,
+                      color: Colors.red,
+                      size: 30,
+                    ),
+                    title: Text("ชื่อโครงการ $index"),
+                    trailing: const Text(
+                      "ปิดโครงการ",
+                      style: TextStyle(
+                          color: Colors.blue,
+                          decoration: TextDecoration.underline),
+                    ),
+                    // subtitle: selectedIndex == index
+                    //     ? const Text("ปิดโครงการ")
+                    //     : null),
+                  ));
+                },
+              ),
             ),
+          ],
+        ),
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.only(bottom: 15.0, right: 15.0),
+          child: FloatingActionButton(
+            onPressed: () {},
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
           ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        ));
   }
 }
